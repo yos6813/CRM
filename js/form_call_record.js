47861e936a760f25b54f9924cccb1a7a1614cef4
yoonjina <yos6813@gmail.com>
2016-11-25 17:15:47 +0900

    161125

--- js/form_call_record.js
+++ js/form_call_record.js
@@ -6,3 +6,26 @@ firebase.database().ref("types/").orderByKey().endAt("type").on("child_added", f
 				+ '</option>');
 	})
 })
+
+
+$("#customerIn").keydown(function(){
+//	var nameInput = $("#customerIn").val();
+	firebase.database().ref("customer/").orderByKey().on("child_added", function(snapshot){
+		firebase.database().ref("customer/" + snapshot.key + '/cusPhone').orderByChild('cusPhone').equalTo($("#customerIn").val()).on('child_added', function(snapshot1){
+			snapshot1.forEach(function(data){
+					alert(data.val());
+				$('#phoneSection').append('<tr>' +
+										  '<td><input type="radio" value="option1" id="optionsContact1" name="optionsContact"></td>' +
+										  '<td>'+ data[0].val() + '</td>' +
+										  '<td>'+ data[1].val() + '</td>' +
+										  '<td class="text-right">' +
+										  '<div class="btn-group">' +
+										  '<button class="modify btn-white btn btn-xs">수정</button>' +
+										  '<button class="delete btn-white btn btn-xs">삭제</button>' +
+										  '</div>' +
+										  '</td>' +
+										  '</tr>');
+			})
+		});
+	});
+});
